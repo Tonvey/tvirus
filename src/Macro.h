997695b8a475511bc9d43ad880dcdb5defa84e30
liangboyan <liangboyanzaz@163.com>
2019-08-08 14:49:14 +0800

    tmp commit

---
+++ src/Macro.h
@@ -0,0 +1,5 @@
+#pragma once
+#define TV_NAME_SPACE tvirus
+#define TV_NAME_SPACE_BEGIN namespace TV_NAME_SPACE {
+#define TV_NAME_SPACE_END }
+#define TV_USING_NAME_SPACE using namespace TV_NAME_SPACE;
---
+++ src/Patcher.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Macro.h"
+#include <string>
+TV_NAME_SPACE_BEGIN
+class Patcher
+{
+public:
+    static Patcher &getInstance(){return smInstance;}
+    void DoPatch(std::string oldFile,std::string newFile,std::string patchFile);
+    void DoDiff(std::string oldFile,std::string newFile,std::string patchFile);
+private:
+    Patcher();
+    Patcher(const Patcher&)=delete;
+    void operator=(const Patcher&)=delete;
+    void operator=(Patcher&&)=delete;
+    static Patcher smInstance;
+};
+TV_NAME_SPACE_END
